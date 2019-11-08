a805be2fcb36013e4a46f2ca9da2a303613566eb
unknown <pawanrealtek@gmail.com>
2019-11-09 00:38:55 +0530

    a

--- quiz/src/reducers/alert.js
+++ quiz/src/reducers/alert.js
@@ -1,6 +1,6 @@
 import {SET_ALERT, REMOVE_ALERT} from '../actions/types'
 
-const initialState = null;
+const initialState = [];
 export default function(state = initialState, action) {
     switch(action.type) {
         case SET_ALERT: 
